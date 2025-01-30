9a4a0749a9421015f1c167348a4af938ec4a0a9b
Husky <husky.robot.dog@gmail.com>
2025-01-31 00:50:24 +0800

    Refactor log layer to improve custom logging implementation
    - Replace existing logging setup with a custom `LogLayer` implementation
    - Enhance log formatting with emoji-based level indicators
    - Simplify log initialization and time formatting logic
    - Improve log timestamp generation and writing mechanism

--- destiny-helpers/src/log.rs
+++ destiny-helpers/src/log.rs
@@ -1,19 +1,11 @@
 use crate::path::cache_dir;
 use anyhow::Result;
+use chrono::{Datelike, Timelike};
 use derive_builder::Builder;
+use std::io::Write;
 use tokio::fs::create_dir_all;
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::{fmt::time::FormatTime, layer::SubscriberExt, Layer};
-
-struct LocalTimer;
-
-impl FormatTime for LocalTimer {
-    fn format_time(&self, w: &mut tracing_subscriber::fmt::format::Writer<'_>) -> std::fmt::Result {
-        let now = chrono::Utc::now()
-            .with_timezone(&chrono::FixedOffset::east_opt(8 * 3600).expect("创建时区偏移失败"));
-        write!(w, "{}", now.format("%Y年%m月%d日%H时%M分%S秒%6f"))
-    }
-}
+use tracing::{level_filters::LevelFilter, Level};
+use tracing_subscriber::{layer::SubscriberExt, Layer};
 
 /// 日志配置
 #[derive(Builder)]
@@ -30,63 +22,88 @@ pub struct LogConfig {
     pub targets: Vec<String>,
 }
 
+struct LogLayer {
+    writers: Vec<tracing_appender::non_blocking::NonBlocking>,
+}
+
+impl LogLayer {
+    pub async fn new(show_std: bool, save_file: bool) -> Result<Self> {
+        let mut writers = vec![];
+        if show_std {
+            let (writer, guard) = tracing_appender::non_blocking(std::io::stdout());
+            writers.push(writer);
+            std::mem::forget(guard);
+        }
+
+        if save_file {
+            let dir = cache_dir()?.join("logs");
+            create_dir_all(&dir).await?;
+            let appender = tracing_appender::rolling::daily(dir, "log");
+            let (writer, guard) = tracing_appender::non_blocking(appender);
+            writers.push(writer);
+            std::mem::forget(guard);
+        }
+
+        Ok(Self { writers })
+    }
+}
+
+impl<S> Layer<S> for LogLayer
+where
+    S: tracing::Subscriber,
+{
+    fn on_event(
+        &self,
+        event: &tracing::Event<'_>,
+        _ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        if self.writers.is_empty() {
+            return;
+        }
+
+        static FIXED_OFFSET: chrono::FixedOffset =
+            chrono::FixedOffset::east_opt(8 * 3600).expect("创建时区偏移失败");
+        let now = chrono::Utc::now().with_timezone(&FIXED_OFFSET);
+        let year = now.year();
+        let month = now.month();
+        let day = now.day();
+        let hour = now.hour();
+        let minute = now.minute();
+        let second = now.second();
+        let millis = now.timestamp_subsec_millis();
+        let micros = now.timestamp_subsec_micros() % 1000;
+
+        let icon = match *event.metadata().level() {
+            Level::TRACE => "🧬",
+            Level::DEBUG => "🔍",
+            Level::INFO => "💬",
+            Level::WARN => "🚨",
+            Level::ERROR => "💥",
+        };
+
+        let msg = format!("{icon} [{year}{month:02}{day:02}][{hour:02}{minute:02}{second:02}][{millis:03}][{micros:03}]\n");
+
+        for out in self.writers.iter() {
+            let mut write = out.clone();
+            write.write_all(msg.as_bytes()).ok();
+        }
+    }
+}
+
 /// 初始化日志, 将设置全局的日志配置.
 /// <br> 重复初始化会报错.
 pub async fn init_log(config: LogConfig) -> Result<()> {
-    let dir = cache_dir()?.join("logs");
-    create_dir_all(&dir).await?;
-
-    let appender = tracing_appender::rolling::daily(dir, "log");
-    let (writer, file_guard) = tracing_appender::non_blocking(appender);
-    let file_layer = tracing_subscriber::fmt::layer()
-        .pretty()
-        .with_level(true)
-        .with_writer(writer)
-        .with_timer(LocalTimer)
-        .with_ansi(false)
-        .with_target(false)
-        .with_file(false)
-        .with_line_number(false)
-        .with_thread_ids(false)
-        .with_thread_names(false)
-        .with_filter(if config.save_file {
-            LevelFilter::INFO
-        } else {
-            LevelFilter::OFF
-        });
-
-    let (writer, std_guard) = tracing_appender::non_blocking(std::io::stdout());
-    let std_layer = tracing_subscriber::fmt::layer()
-        .pretty()
-        .with_level(true)
-        .with_writer(writer)
-        .with_timer(LocalTimer)
-        .with_target(false)
-        .with_file(false)
-        .with_line_number(false)
-        .with_thread_ids(false)
-        .with_thread_names(false)
-        .with_filter(if config.show_std {
-            LevelFilter::TRACE
-        } else {
-            LevelFilter::OFF
-        });
-
     let mut targets =
         tracing_subscriber::filter::Targets::new().with_target("destiny_", LevelFilter::TRACE);
     for target in config.targets {
         targets = targets.with_target(target, LevelFilter::TRACE);
     }
 
-    let collector = tracing_subscriber::registry()
-        .with(targets)
-        .with(file_layer)
-        .with(std_layer);
+    let layer = LogLayer::new(config.show_std, config.save_file).await?;
+
+    let collector = tracing_subscriber::registry().with(targets).with(layer);
 
     tracing::subscriber::set_global_default(collector)?;
-
-    std::mem::forget(file_guard);
-    std::mem::forget(std_guard);
 
     Ok(())
 }
